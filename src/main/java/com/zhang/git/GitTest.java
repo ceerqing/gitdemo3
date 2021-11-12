924f2b878a325e35f7728d644985e0ff0abe5e57
ceerqing <94104808+ceerqing@users.noreply.github.com>
2021-11-12 21:41:55 +0800

    pulltest

--- src/main/java/com/zhang/git/GitTest.java
+++ src/main/java/com/zhang/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hotfix add");
         System.out.println("share test");
         System.out.println("push test");
+         System.out.println("pull test");
     }
 }
