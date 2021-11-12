6519f9245c0d33df5a680f65f63e0e730d4bf307
zhang <2519601764@qq.com>
2021-11-12 20:49:10 +0800

    hotfix add

--- src/main/java/com/zhang/git/GitTest.java
+++ src/main/java/com/zhang/git/GitTest.java
@@ -6,5 +6,6 @@ public class GitTest {
         System.out.println("git hello2");
         System.out.println("git hello3");
         System.out.println("hot git hello");
+        System.out.println("hotfix add");
     }
 }
