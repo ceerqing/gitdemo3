2d6b19d316208103fd56751793458453e31007be
zhang <2519601764@qq.com>
2021-11-12 20:28:56 +0800

    two commit

--- src/main/java/com/zhang/git/GitTest.java
+++ src/main/java/com/zhang/git/GitTest.java
@@ -3,5 +3,6 @@ package com.zhang.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("git hello");
+        System.out.println("git hello2");
     }
 }
